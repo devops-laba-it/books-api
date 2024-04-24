0ceecc1827e510f82fe9e498cc3bc062928dafd2
Paweł Piech <studiapawelp@gmail.com>
2024-04-24 13:34:51 +0200

    style(lambda_function): reformat with black

--- lambda_function.py
+++ lambda_function.py
@@ -8,23 +8,26 @@ import uuid
 
 app = Flask(__name__)
 
-AWS_ACCESS_KEY_ID = os.environ.get('BOOKS_AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('BOOKS_AWS_SECRET_ACCESS_KEY')
-AWS_REGION = os.environ.get('BOOKS_AWS_REGION')
-TABLE_NAME = os.environ.get('BOOKS_TABLE_NAME')
-queue_url = os.environ.get('BOOKS_QUEUE_URL')
-IMAGE_API_URL = os.environ.get('BOOKS_IMAGE_API_URL')
+AWS_ACCESS_KEY_ID = os.environ.get("BOOKS_AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("BOOKS_AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.environ.get("BOOKS_AWS_REGION")
+TABLE_NAME = os.environ.get("BOOKS_TABLE_NAME")
+queue_url = os.environ.get("BOOKS_QUEUE_URL")
+IMAGE_API_URL = os.environ.get("BOOKS_IMAGE_API_URL")
 
 sqs = boto3.client(
-    service_name='sqs',
+    service_name="sqs",
     region_name=AWS_REGION,
     aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
 
-dynamodb = boto3.resource('dynamodb',
-                          region_name=AWS_REGION,
-                          aws_access_key_id=AWS_ACCESS_KEY_ID,
-                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+dynamodb = boto3.resource(
+    "dynamodb",
+    region_name=AWS_REGION,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
 
 table = dynamodb.Table(TABLE_NAME)
 
@@ -47,46 +50,47 @@ def send_to_queue(book: Book):
     return response
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        url = request.form['url']
-        author = request.form['author']
-        title = request.form['title']
+    if request.method == "POST":
+        url = request.form["url"]
+        author = request.form["author"]
+        title = request.form["title"]
 
-        url = url if url else 'https://picsum.photos/2000'
+        url = url if url else "https://picsum.photos/2000"
 
         book = Book(id=None, title=title, author=author, url=url)
 
         table.put_item(
             Item={
-                'id': book.id,
-                'author': book.author,
-                'title': book.title,
+                "id": book.id,
+                "author": book.author,
+                "title": book.title,
             }
         )
 
         response = send_to_queue(book)
         print(response)
 
-        return render_template('index.html', book=book)
+        return render_template("index.html", book=book)
 
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/books')
+@app.route("/books")
 def all_books():
     books = []
-    for item in table.scan().get('Items'):
+    for item in table.scan().get("Items"):
         print(item)
-        id = item.get('id')
+        id = item.get("id")
         book = Book(
             id=id,
-            title=item.get('title'),
-            author=item.get('author'),
-            url=get_image_url(id))
+            title=item.get("title"),
+            author=item.get("author"),
+            url=get_image_url(id),
+        )
         books.append(book)
-    return render_template('all_books.html', books=books)
+    return render_template("all_books.html", books=books)
 
 
 def get_image_url(book_id):
@@ -94,13 +98,13 @@ def get_image_url(book_id):
 
 
 def lambda_handler(event, context):
-    event['httpMethod'] = event['requestContext']['http']['method']
-    event['path'] = event['requestContext']['http']['path']
-    event['queryStringParameters'] = event.get('queryStringParameters', {})
+    event["httpMethod"] = event["requestContext"]["http"]["method"]
+    event["path"] = event["requestContext"]["http"]["path"]
+    event["queryStringParameters"] = event.get("queryStringParameters", {})
 
     return awsgi.response(app, event, context)
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
